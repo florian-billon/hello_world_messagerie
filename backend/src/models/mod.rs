443c97d3c901fda53c17eeb4894b52abb54371c3
florian-billon <154011027+florian-billon@users.noreply.github.com>
2026-04-10 12:27:19 +0200

    commit 10.04.2026 12h27

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,13 +1,13 @@
-pub mod dm;
 pub mod channel;
+pub mod dm;
 pub mod invite;
 pub mod message;
 pub mod server;
 pub mod user;
 
 pub use channel::*;
+pub use dm::*;
 pub use invite::*;
 pub use message::*;
 pub use server::*;
 pub use user::*;
-pub use dm::*;
