a43eb628a87ed353356242ceb164a6b752550b64
florian-billon <154011027+florian-billon@users.noreply.github.com>
2026-04-15 18:02:15 +0200

    commit 15.04.2026 18h02

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -3,9 +3,9 @@ pub mod channels;
 pub mod invites;
 pub mod messages;
 pub mod servers;
+pub mod dm; // <--- 1. Ajoute cette ligne (ou 'pub mod conversations' selon le nom du fichier)
 
 use axum::Router;
-
 use crate::AppState;
 
 pub fn create_router() -> Router<AppState> {
@@ -14,4 +14,5 @@ pub fn create_router() -> Router<AppState> {
         .merge(channels::routes())
         .merge(messages::routes())
         .merge(invites::routes())
+        .merge(dm::routes()) // <--- 2. Ajoute cette ligne pour inclure les routes de DM
 }
